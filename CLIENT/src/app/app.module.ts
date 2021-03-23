394c330ebc4fb79882ca1e1073e5db4797f165ac
MG7 <matheus.campos@edu.unifil.br>
2021-03-23 20:14:16 -0300

    feat: projeto criado.

---
+++ CLIENT/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
