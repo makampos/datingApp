8acf20cb64747d8e0ff780eba29be65f61c5d9ce
MG7 <matheus.campos@edu.unifil.br>
2021-03-23 23:50:04 -0300

    refactor: criado extension methods

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,10 +1,17 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
 
 namespace API
 {
@@ -21,16 +28,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           
-
-            services.AddDbContext<DataContext>(options => 
-            {
-                options.UseMySql(_config.GetConnectionString("DefaultConnection"));
-            });
+            services.AddApplicationServices(_config);
             services.AddControllers();
-
             services.AddCors();
-           
+            services.AddIdentityServices(_config);                       
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +50,7 @@ namespace API
             .AllowAnyMethod()
             .WithOrigins("https://localhost:4200"));
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
