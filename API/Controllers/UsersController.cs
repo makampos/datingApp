778a8cfcf1a668b2747c4790d7459fa8cb2f4aa1
MG7 <matheus.campos@edu.unifil.br>
2021-03-25 01:08:20 -0300

    feat: implementação de login e logout no front-end e localstorage para guardar sessão

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -19,7 +19,7 @@ namespace API.Controllers
             
         }
 
-        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
--- CLIENT/src/app/app.module.ts
+++ CLIENT/src/app/app.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule    
+    BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
