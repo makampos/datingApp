e44f202b8c77e778b6e2c2992954fc7b075b0b58
MG7 <matheus.campos@edu.unifil.br>
2021-04-04 20:53:04 -0300

    feat: criado classe de serviço para membros

--- CLIENT/src/app/_services/account.service.ts
+++ CLIENT/src/app/_services/account.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { ReplaySubject } from 'rxjs';
 import {map} from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/User';
 
 
@@ -10,7 +11,7 @@ import { User } from '../_models/User';
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/'
+  baseUrl = environment.apiUrl;
   
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
