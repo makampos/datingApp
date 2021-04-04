6043eb3e89d25509940294ed23d206cc61b40c8f
MG7 <matheus.campos@edu.unifil.br>
2021-04-04 20:51:05 -0300

    feat: nova migration

---
+++ API/Data/Migrations/20210404233425_correcao_atributo.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class correcao_atributo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KnowAs",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "KnownAs",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KnownAs",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "KnowAs",
+                table: "Users",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+    }
+}
