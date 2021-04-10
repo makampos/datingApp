6a9c8b580413cd41b9ffc37e960f3b1e3cc76018
MG7 <matheus.campos@edu.unifil.br>
2021-04-10 14:13:47 -0300

    feat: adicionado biblioteca de galeria de imagem e tabs

--- CLIENT/src/app/_modules/shared.module.ts
+++ CLIENT/src/app/_modules/shared.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
-
-
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [],
@@ -11,12 +11,16 @@ import { ToastrModule } from 'ngx-toastr';
     CommonModule,
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
-      positionClass: 'toast-top-right'
+      positionClass: 'toast-top-right',
     }),
+    TabsModule.forRoot(),
+    NgxGalleryModule
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
+    TabsModule,
+    NgxGalleryModule
   ]
 })
 export class SharedModule { }
